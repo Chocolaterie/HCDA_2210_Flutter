06c0bf67bfd8448b8517c0e4059db80cddcaedea
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-06-24 16:50:16 +0200

    Correction tp 1 - p3

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const message =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris n";
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -35,7 +38,63 @@ class HomePage extends StatelessWidget {
         body: Column(
           children: [
             HeaderComponent(),
-            Expanded(child: Text("TODO C'est pour plus tard"),),
+            Expanded(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: SizedBox(
+                      height: 150,
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Image.network(
+                                      "https://media.tenor.com/-xjD3-jZ61IAAAAM/chemmsuwuowo.gif",
+                                      width: 145,
+                                    ),
+                                    Expanded(
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Text("Shiba@gmail.com"),
+                                              Text("56s")
+                                            ],
+                                          ),
+                                          Expanded(child: Text(message))
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Text("Répondre"),
+                                    Text("Retweet"),
+                                    Text("Favoris"),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             FooterComponent(),
           ],
         ));
