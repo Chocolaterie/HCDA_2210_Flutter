4729a22616a58e5994bdecb4aaede7e1ef53be7e
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2024-06-26 15:04:53 +0200

    Correction Tp 5 p 1

--- lib/home/home-page.dart
+++ lib/home/home-page.dart
@@ -8,7 +8,6 @@ import 'package:http/http.dart' as http;
 import 'package:tp_twitter/home/tweet.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -20,6 +19,11 @@ class _HomePageState extends State<HomePage> {
    * Va appeler l'api
    */
   void callApi(BuildContext context) async {
+    // Clear des messages
+    setState(() {
+      tweets = [];
+    });
+
     // Afficher chargement
     AppAlertMgr().showProgress(context, "Récupérer des tweets");
 
@@ -27,22 +31,43 @@ class _HomePageState extends State<HomePage> {
     await Future.delayed(Duration(seconds: 1));
 
     // Preparer l'url
-    var url = Uri.parse("https://raw.githubusercontent.com/Chocolaterie/EniWebService/main/api/tweets.json");
+    var url = Uri.parse("http://localhost:3000/v2/comment/all");
+
+    // Preparer le header
+    final headers =
+    {
+      'Authorization': 'Bearer dfIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImlzYWFjQGdtYWlsLmNvbSIsImlhdCI6MTcxOTQwNjgxNiwiZXhwIjoxNzE5NDEwNDE2fQ.AJobIrr6Yb307VOc5KHB5mB1F05mgl-RZu6cw8ih83Q'
+    };
 
     // Appeler l'api
-    var response = await http.get(url);
+    var response = await http.get(url, headers: headers);
 
     // convertir le body e njson
     var json = convert.jsonDecode(response.body);
 
+    // Fermer le popup de chargement (dans tout les cas aprés l'API)
+    AppAlertMgr().closeProgress();
+
+    // Si erreur token
+    if (json['code'] == '799' || json['code'] == '798') {
+      // Afficher l'erreur metier
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Erreur'),
+              content: Text(json['message'] as String),
+            );
+          });
+      return;
+    }
+
     // convertir le json en liste de tweets
-    tweets = List<Tweet>.from(json.map((tweetJson) => Tweet.fromJson(tweetJson)).toList());
+    tweets = List<Tweet>.from(
+        json['data'].map((tweetJson) => Tweet.fromJson(tweetJson)).toList());
 
     // rafraichir la page
     setState(() {});
-
-    // Fermer le popup de chargement
-    AppAlertMgr().closeProgress();
   }
 
   @override
@@ -74,8 +99,8 @@ class _HomePageState extends State<HomePage> {
                     child: ListView.builder(
                         itemCount: tweets.length,
                         itemBuilder: (BuildContext context, int index) {
-                      return TweetCard(tweets[index]);
-                    }),
+                          return TweetCard(tweets[index]);
+                        }),
                   ),
                 ],
               ),
